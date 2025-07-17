45e3968831a588ee5b01c28e1b659ce7c458df51
Danilo <danilosantossilveira.com@gmail.com>
2025-07-17 00:16:36 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+//desafio 1
+
+
+
+/*let titulo = document.querySelector ("h1");
+titulo.innerHTML = "Hora do desafio";
+function botaoclick(){
+    console.log ("O botão foi clicado")
+}*/
+
+
+
+/*function botaoclick(){
+    alert ("Eu amo JS");
+}*/
+
+
+
+/*function botaoclick(){
+    let cidade =prompt();
+    alert (`Estive em ${cidade} e lembrei de você`);
+    }*/
+
+
+   
+//Ao clicar no botão soma, peça 2 números inteiros e exiba o resultado da soma em um alerta.
