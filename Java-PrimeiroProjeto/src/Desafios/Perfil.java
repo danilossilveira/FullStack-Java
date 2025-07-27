694b74a607b69930999cf9bc81ed32793e0b35c4
Danilo <danilosantossilveira.com@gmail.com>
2025-07-27 17:52:08 -0300

    Add files via upload

---
+++ Java-PrimeiroProjeto/src/Desafios/Perfil.java
@@ -0,0 +1,11 @@
+package Desafios;
+
+public class Perfil {
+    public static void main(String[] args) {
+        String nome = "Danilo";
+
+        System.out.println("Olá, " + nome + "!");
+   System.out.println("Tudo bem?");
+    }
+}
+
---
+++ Java-PrimeiroProjeto/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao Scream math");
+        System.out.println("Filme: Carros 2");
+        int anoDeLançamento = 2011;
+        System.out.println("Este fime foi lançado em:"+anoDeLançamento);
+        boolean inclusoNoPlano=true;
+        double notaDoFilme=5.7;
+    }
+}
