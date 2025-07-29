34b3c473eb96d1d1cce13a5398398fb338b41a8f
Danilo <danilosantossilveira.com@gmail.com>
2025-07-29 11:39:19 -0300

    Add files via upload

---
+++ ProjetoJava/Estudos java/Java-PrimeiroProjeto/src/Desafios/Soma.java
@@ -0,0 +1,15 @@
+package Desafios;
+
+public class Soma {
+  static int valor1 = 5+11;
+    public static void main(String[] args) {
+
+        System.out.println(valor1);
+    }
+}
+
+
+
+//Crie uma classe Soma em Java para imprimir no console o resultado da operação 10 + 5.
+// Certifique-se de que o resultado seja exibido sem o uso de aspas, apresentando o valor "15"
+// ao invés da expressão "10+5".
