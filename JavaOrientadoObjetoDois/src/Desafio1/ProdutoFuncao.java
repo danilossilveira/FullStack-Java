141cc035f7485a69518c9aa69a3310359073cc64
Danilo <danilosantossilveira.com@gmail.com>
2025-08-06 09:50:25 -0300

    Add files via upload

---
+++ JavaOrientadoObjetoDois/src/Desafio1/ProdutoFuncao.java
@@ -0,0 +1,11 @@
+package Desafio1;
+
+public class ProdutoFuncao {
+
+}
+
+
+
+//Desenvolva uma classe Produto com os atributos privados nome e preco.
+// Utilize métodos getters e setters para acessar e modificar esses atributos.
+// Adicione um método aplicarDesconto que recebe um valor percentual e reduz o preço do produto.
