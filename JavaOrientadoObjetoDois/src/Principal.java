48515bd71761ea835b478e3226a42ddc2b5b5a9a
Danilo <danilosantossilveira.com@gmail.com>
2025-08-07 11:25:11 -0300

    Add files via upload

--- JavaOrientadoObjetoDois/src/Principal.java
+++ JavaOrientadoObjetoDois/src/Principal.java
@@ -1,9 +1,10 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.setNomeDoFilme("O rei leão");
+       /* Filme meuFilme = new Filme();
+        meuFilme.setNome("O rei leão");
         meuFilme.setAnoDeLancamento(1994);
         meuFilme.setMinutosDeDuracao(130);
 
@@ -11,11 +12,17 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(9);
         meuFilme.avalia(10);
-//        System.out.println(meuFilme.setSomaDasAvaliacoes());
-//        System.out.println(meuFilme.setTotalDasAvaliacoes());
-        System.out.println(meuFilme.somaMedia());
+        System.out.println(meuFilme.somaMedia());*/
 
+        Serie minhaSerie = new Serie();
+        minhaSerie.setNome("The Walking Dead");
+        minhaSerie.setAnoDeLancamento(2010);
+        minhaSerie.fichaTecnica();
+
+        minhaSerie.setTemporadas(11);
+        minhaSerie.setEpisodioPorTemporada(15);
+        minhaSerie.setAtiva(true);
+        minhaSerie.setMinutosPorEpisodio(50);
+        System.out.println(minhaSerie.getMinutosDeDuracao() + "minutos");
     }
 }
-
-//import br.com.alura.screenmatch.modelos.Filme;
--- JavaOrientadoObjetoDois/src/br/com/alura/screenmatch/modelos/Filme.java
+++ JavaOrientadoObjetoDois/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,47 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nomeDoFilme;
-    private int anoDeLancamento;
-    private boolean inclusoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDasAvaliacoes;
-    private int minutosDeDuracao;
+public class Filme extends Titulo{
+   private String diretor;
 
-
-
-    public void setNomeDoFilme(String nomeDoFilme) {
-        this.nomeDoFilme = nomeDoFilme;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setTotalDasAvaliacoes(int totalDasAvaliacoes) {
-        this.totalDasAvaliacoes = totalDasAvaliacoes;
-    }
-
-    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
-        this.somaDasAvaliacoes = somaDasAvaliacoes;
-    }
-
-    public void setMinutosDeDuracao(int minutosDeDuracao) {
-        this.minutosDeDuracao = minutosDeDuracao;
-    }
-
-    public void fichaTecnica(){
-        System.out.println("O nome do filme é: "+ nomeDoFilme);
-        System.out.println("O ano de lançamento é: "+ anoDeLancamento);
-        System.out.println("O filme tem " + minutosDeDuracao + " minutos");
-    }
-
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDasAvaliacoes++;
-    }
-
-    public double somaMedia() {
-        return somaDasAvaliacoes / totalDasAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
