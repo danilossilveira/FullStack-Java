1bca798619a87292fdc92a9cb72744bdc77c8f23
Danilo <danilosantossilveira.com@gmail.com>
2025-09-22 10:16:57 -0300

    Update Teste.java

--- DesafioArrayList/src/Teste.java
+++ DesafioArrayList/src/Teste.java
@@ -1,9 +1,1 @@
-public class Teste extends Lists{
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-}
 
-
-// 2. Iterar sobre o ArrayList e comparar as strings
